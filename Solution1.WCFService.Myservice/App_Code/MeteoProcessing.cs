2a404269e44a8799d0cd75d0eb924e233deb938f
SDIV30 <stepanov.vanya2013@gmail.com>
2023-11-21 00:29:15 +0300

    Improved performance
    added new methods that send data through delegates

--- Solution1.WCFService.Myservice/App_Code/MeteoProcessing.cs
+++ Solution1.WCFService.Myservice/App_Code/MeteoProcessing.cs
@@ -11,41 +11,53 @@ using System.Web;
 public delegate void SendPogoda(Pogoda pog);
 public class MeteoProcessing
 {
+    
+    private delegate Task<Pogoda> SendToFunc(double temperature, double windDirection, int count, DateTime sortBegin, int i);
+
     public MeteoProcessing()
     {
     }
     public SendPogoda sendResult;
-    
-    public void AveragingBegin(int timeInterval, List<Pogoda> sortedResult) 
+    private SendToFunc sendToFunc;
+
+
+    public void AveragingBegin(int timeInterval, List<Pogoda> sortedResult)
     {
         List<Task<Pogoda>> tasks = new List<Task<Pogoda>>();
+        List<Task<bool>> vartasks = new List<Task<bool>>();
+
         List<Pogoda> averagedData = new List<Pogoda>();
         var sortEnd = sortedResult[0].Dat.AddSeconds(timeInterval);
         var sortBegin = sortedResult[0].Dat;
         var count = 0;
         double temperature = 0;
         double windDirection = 0;
-
         Stopwatch sw = new Stopwatch();
+
+
         sw.Start();
         for (int i = 0; i < sortedResult.Count; i++)
-        {
+        {//функция начало конец индекс и потом калк таск
+            //if в таск
             if (sortedResult[i].Dat < sortEnd)
             {
-                count++;
-                temperature += sortedResult[i].Temp;
-                windDirection += sortedResult[i].WindDir;
+                if (sortedResult[i].WindDir!= -1)
+                {
+                    count++;
+                    temperature += sortedResult[i].Temp;
+                    windDirection += sortedResult[i].WindDir;
+                }
             }
             
             else
             {
-                //System.Diagnostics.Debug.WriteLine("task launched "+ i);
-                //var pog = PogodaCalcTask(temperature,windDirection,count,sortBegin);
-                //averagedData.Add(pog.Result);
-                System.Diagnostics.Debug.WriteLine("task launched "+ i);
-                tasks.Add(PogodaCalcAsync(temperature, windDirection, count, sortBegin,i));//
-                //Task.Run(async ()=>await PogodaCalcAsync(temperature, windDirection, count, sortBegin, i));
-                System.Diagnostics.Debug.WriteLine("step over");
+                //await all 
+                if (count != 0) 
+                {
+                    System.Diagnostics.Debug.WriteLine("task launched "+ i);
+                    tasks.Add(PogodaCalcAsync(temperature, windDirection, count, sortBegin,i));
+                    System.Diagnostics.Debug.WriteLine("step over");
+                }
                 temperature =0;
                 windDirection=0;
                 count = 0;
@@ -53,16 +65,21 @@ public class MeteoProcessing
                 sortEnd = sortEnd.AddSeconds(timeInterval);
             }
         }
+        /*
+        sendToFunc +=PogodaCalcAsync;//работает
+        Logics(sortedResult, sortBegin, sortEnd, timeInterval);*/ 
+        
         sw.Stop();
-        Task.WaitAll(tasks.ToArray());
 
+        Task.WaitAll(tasks.ToArray());
         System.Diagnostics.Debug.WriteLine("Execution took "+sw.ElapsedMilliseconds+"ms");
     }
+    
     //придумать что то для асинка
     private async Task<Pogoda> PogodaCalcAsync(double temperature, double windDirection, int count, DateTime sortBegin,int id) {
         System.Diagnostics.Debug.WriteLine("task started " + id);
         var res = await PogodaCalcTask(temperature, windDirection, count, sortBegin);
-        sendResult.Invoke(res);
+        //sendResult.Invoke(res);
         System.Diagnostics.Debug.WriteLine("task finished "+id);
         return res;
     }
@@ -84,6 +101,45 @@ public class MeteoProcessing
             Temp = Math.Round(averageTemperature, 1),
             WindDir = Math.Round(averageWindDirection, 1)
         };
+        sendResult.Invoke(resultPogoda);//
         return resultPogoda;
     }
+    private bool Logics(List<Pogoda> sortedResult, DateTime sortBegin, DateTime sortEnd, int timeInterval)
+    {//функция начало конец индекс и потом калк таск
+        int count = 0;
+        double temperature = 0;
+        double windDirection = 0;
+        for (int i = 0; i < sortedResult.Count; i++)
+        {
+            if (sortedResult[i].Dat < sortEnd)
+            {
+                if (sortedResult[i].WindDir != -1)
+                {
+                    //через делегаты
+                    count++;
+                    temperature += sortedResult[i].Temp;
+                    windDirection += sortedResult[i].WindDir;
+                }
+            }
+            else 
+            {
+                if (count != 0) 
+                {
+                    sendToFunc.Invoke(temperature, windDirection,count,sortBegin,i);
+                }
+                sortBegin = sortEnd;
+                sortEnd = sortEnd.AddSeconds(timeInterval);
+            }
+        }
+        return true;
+    }
+    private Task<bool> LogicsTask(List<Pogoda> sortedResult, DateTime sortBegin, DateTime sortEnd, int timeInterval) {
+        var res = Task.Run(() => Logics(sortedResult,sortBegin,sortEnd,timeInterval));
+        return res;
+    }
+    private async Task<bool> LogicAsync(List<Pogoda> sortedResult, DateTime sortBegin, DateTime sortEnd, int timeInterval) {
+        var res = await LogicsTask(sortedResult, sortBegin, sortEnd, timeInterval);
+        return res;
+    }
+
 }
